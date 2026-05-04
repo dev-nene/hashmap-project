85cc531de07d3c1989799bc4f3593ae31226d213
Nemanja Pavlovic <268587348+dev-nene@users.noreply.github.com>
2026-05-04 21:58:11 +0200

    Add set method

--- hashMap.js
+++ hashMap.js
@@ -3,18 +3,57 @@ class HashMap {
     this.loadFactor = 0.75;
     this.capacity = 16;
     this.size = 0;
-    this.buckets = new Array(this.capacity).fill(null);
+    this.buckets = new Array(this.capacity);
   }
 
   hash(key) {
     let hashCode = 0;
-
     const primeNumber = 31;
+
     for (let i = 0; i < key.length; i++) {
-      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+      hashCode =
+        (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
     }
+
     return hashCode;
   }
 
+  set(key, value) {
+    const index = this.hash(key);
 
+    if (!this.buckets[index]) {
+      this.buckets[index] = [[key, value]];
+      this.size++;
+    } else {
+      for (let pair of this.buckets[index]) {
+        if (pair[0] === key) {
+          pair[1] = value;
+          return;
+        }
+      }
+
+      this.buckets[index].push([key, value]);
+      this.size++;
+    }
+
+    if (this.size / this.capacity > this.loadFactor) {
+      this.resize();
+    }
+  }
+
+  resize() {
+    const oldBuckets = this.buckets;
+
+    this.capacity *= 2;
+    this.buckets = new Array(this.capacity);
+    this.size = 0;
+
+    for (let bucket of oldBuckets) {
+      if (!bucket) continue;
+
+      for (let [key, value] of bucket) {
+        this.set(key, value);
+      }
+    }
+  }
 }
